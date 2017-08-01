a3efefa3dcf5f2649c34627638d05c1ba206566d
impiyushs <pgsupe@gmail.com>
2017-08-01 13:22:05 -0500

    Fibonacci series using For loop in python

---
+++ Fibonacci_loop.py
@@ -0,0 +1,18 @@
+function getFib(position) {
+  if (position == 0) { return 0; }
+  if (position == 1) { return 1; }
+  var first = 0,
+      second = 1,
+      next = first + second;
+  for (var i = 2; i < position; i++) {
+    first = second;
+    second = next;
+    next = first + second;
+  }
+  return next;
+}
+
+# Test cases
+print getFib(9)
+print getFib(11)
+print getFib(0)
